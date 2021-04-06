260dca1699bf206c796d030d69d29e4626691f02
Francisco Serrano <8388391+FranciscoSerrano@users.noreply.github.com>
2021-04-06 15:05:09 -0400

    added josh's histogram implementation

--- chapter-1/1.1/exercises/1.1.15.py
+++ chapter-1/1.1/exercises/1.1.15.py
@@ -4,7 +4,18 @@ m as arguments and returns an array of length m whose ith entry is the number of
 that integer i appeared in the argument array. If the values in are all between 0 and m-1,
 the sum of the values in the returned array should be equal to a.length.
 '''
+'''
+Time Complexity
+Size of the problem: size of the inputs
+Pay attention to all inputs. In this case array and int.
 
+Opportunities:
+count +=1 happens len(arr) times * int times
+
+So my histogram is O(len(arr)*int)
+
+So Josh's histogram is O(m + len(data))
+'''
 
 def histogram(arr, intt):
     hist = []
@@ -20,6 +31,26 @@ def histogram(arr, intt):
         hist.append(count)
     return hist
 
+# Create a histogram of integer values from 0 to m
+def histogram_josh(data, m):
+    # Create bins to keep track of how frequently
+    # we encounter a value.
+    # NOTE: At the beginning we haven't looked at any of
+    # the data, so all the counts are zero.
+    # Since we're keeping track of values from 0 to m, we need m + 1 bins.
+    bins = [0] * (m + 1)
+    for element in data:
+        # Check to see if the current element is part of
+        # the distribution we are counting
+        if element > m:
+            continue
+        # Note that we saw another value of element
+        bins[element] += 1
+    # We counted everything. Return the bins
+    return bins
+
+
 array = [1, 2, 4, 5, 6, 7, 2, 3, 4, 7, 7, 7 , 8, 2, 1]
 
 print(histogram(array, 8))
+print(histogram_josh(array, 8))
